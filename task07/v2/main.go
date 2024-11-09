055c542f8cb0fef562401833afa5beb11f7fc637
vldkhu <evirus777@yandex.ru>
2024-11-09 22:49:10 +0500

    add solution task07 add commets

--- task07/v2/main.go
+++ task07/v2/main.go
@@ -7,20 +7,21 @@ import (
 )
 
 func main() {
-	var m sync.Map
+	var m sync.Map // Создаем sync.Map для безопасного доступа
 
-	start := time.Now()
+	start := time.Now() // Запоминаем время начала
 
+	// Заполняем sync.Map значениями от 0 до 9999
 	for i := 0; i < 10_000; i++ {
-		m.Store(i, i)
+		m.Store(i, i) // Сохраняем ключ и значение
 	}
 
+	// Выводим все элементы из sync.Map
 	m.Range(func(key, value any) bool {
-		fmt.Println(key, value)
-		return true
+		fmt.Println(key, value) // Печатаем ключ и значение
+		return true             // Продолжаем итерацию
 	})
 
-	elapsed := time.Since(start)
-	fmt.Printf("Время чтения: %s\n", elapsed)
-
+	elapsed := time.Since(start)              // Вычисляем время выполнения
+	fmt.Printf("Время чтения: %s\n", elapsed) // Выводим время
 }
